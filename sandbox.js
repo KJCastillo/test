32bf788433a519d02c815639decef0990c8672d5
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-01 17:16:19 -0800

    testing arrays

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,12 @@
-console.log('js working')
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken'
+// console.log(ninjas);
+
+// let ages = [20,25,30,35];
+
+// console.log(ages[2]);
+
+let result = ninjas
+
+console.log(result)
