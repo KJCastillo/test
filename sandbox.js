21f5cf99cc954d7fb38fb9391f56c61248b45e58
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-02 15:46:26 -0800

    testing while loops

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,2 @@
-//for loops
+//while loops
 
-// for(let i =0; i <5; i++){
-//    console.log('in loop:', i)
-// }
-
-// console.log('loop finished');
-
-const names = ['shaun', 'mario', 'luigi'];
-
-for(let i =0; i < names.length; i++){
-    
-}
