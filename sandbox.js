60d46c6242955641a3535494987da0c91fe49901
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-05 11:44:53 -0800

    arrow functions

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,20 @@
 
-const speak = function(name){
-    console.log(`hello ${name}`)
+// const speak = function(name, last){
+//     console.log(`hello ${name} ${last}`)
+// };
+
+// speak('kevin', 'castillo');
+
+// function calcArea(radius){
+//     return 3.14 * radius**2;
+    
+// }
+
+const calcArea = (radius) => {
+    return 3.14 * radius**2
 };
 
-speak('kevin');
+
+
+const area = calcArea(5);
+console.log('area is:', area)
