ff92869989354e0fd739592ce26ddb91fc3cc6b9
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-06 11:30:13 -0800

    math object

--- sandbox.js
+++ sandbox.js
@@ -7,29 +7,42 @@
 
 // console.log(blogs)
 
-let user = {
-    name: 'kevin',
-    age: 31,
-    email: 'kjc@gmail.com',
-    location: 'san diego',
-    blogs: [
-        {title: 'coding', likes: 3},
-        {title: 'bjj', likes: 18}
-    ],
-    login: function(){
-        console.log('user logged in');
-    },
-    logout: function(){
-        console.log('user logged out');
-    },
-    logBlogs(){
-        console.log('user has written the following blogs');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes)
-        })
-    }
-};
+// let user = {
+//     name: 'kevin',
+//     age: 31,
+//     email: 'kjc@gmail.com',
+//     location: 'san diego',
+//     blogs: [
+//         {title: 'coding', likes: 3},
+//         {title: 'bjj', likes: 18}
+//     ],
+//     login: function(){
+//         console.log('user logged in');
+//     },
+//     logout: function(){
+//         console.log('user logged out');
+//     },
+//     logBlogs(){
+//         console.log('user has written the following blogs');
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes)
+//         })
+//     }
+// };
 
-user.login();
-user.logout();
-user.logBlogs();
+// user.login();
+// user.logout();
+// user.logBlogs();
+
+//Math Object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.5;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
