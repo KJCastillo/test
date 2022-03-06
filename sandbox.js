d5c12ffe59b336c932c9d9ff31e96aeeaeb15690
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-06 11:26:22 -0800

    object literal functions

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,35 @@
 //object literal
 
+// const blogs = [
+//     {title: 'coding', likes: 3},
+//     {titile: 'bjj', likes: 18}
+// ];
+
+// console.log(blogs)
+
 let user = {
     name: 'kevin',
     age: 31,
     email: 'kjc@gmail.com',
     location: 'san diego',
-    blogs: ['coding', 'bjj']
+    blogs: [
+        {title: 'coding', likes: 3},
+        {title: 'bjj', likes: 18}
+    ],
+    login: function(){
+        console.log('user logged in');
+    },
+    logout: function(){
+        console.log('user logged out');
+    },
+    logBlogs(){
+        console.log('user has written the following blogs');
+        this.blogs.forEach(blog => {
+            console.log(blog.title, blog.likes)
+        })
+    }
 };
 
+user.login();
+user.logout();
+user.logBlogs();
