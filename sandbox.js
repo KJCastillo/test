606e51a4fe183fb58e6b410d5cd8c9847430e058
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-09 15:55:54 -0800

    dom

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,1 @@
-// let scoreOne = 50;
-// let scoreTwo = scoreOne;
-
-// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
-
-// scoreOne = 100;
-
-// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
-
-const userOne = {name: 'ryu', age: 30};
-
-const userTwo = userOne;
-
-console.log(userOne, userTwo);
-
-userOne.age = 40;
-
-console.log(userOne, userTwo);
+const para = document.querySelector("p");
