93ee2fd8fcd2e6774c96baa9f08f1960f99b80d6
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-09 16:52:19 -0800

    dom style

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('h1');
+
+console.log(title.style)
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+
+title.style.fontSize = '60px';
+title.style.margin = '';
