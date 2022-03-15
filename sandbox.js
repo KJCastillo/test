d6a3c89e67fb7df1e1bd6ba0f64f8ce7abf3c5d8
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-14 17:24:42 -0700

    form event listeners

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('.signup-form');
-//const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
+const username = form.username.value;
+const usernamePattern = /^[a-zA-z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const username = form.username.value;
-    const usernamePattern = /^[a-zA-z]{6,12}$/;
-    
+    if(usernamePattern.test(username)){
+        feedback.textContent = 'username valid'
+    }else {
+        feedback.textContent = 'username invalid'
+    }
 });
 
+form.username.addEventListener('keyup', e => {
+    if(usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class', 'success')
+    } else {
+        form.username.setAttribute('class', 'error')
+    }
+});
