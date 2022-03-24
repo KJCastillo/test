3f730b99ffd23fe54e97b96c7c3663f0279fd0ad
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-24 13:49:15 -0700

    reduce method

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,19 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-const users = [
-    {name: "kevin", premium: true},
-    {name: 'castillo', premium: false},
-    {name: 'penny', premium: false},
-    {name: 'daisy', premium: true}
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shell', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shell', price: 50}
 ];
 
-const x = users.filter(user => {
-    return user.premium
+let x = products.map((product) => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2}
+    } else {
+        return product
+    }
 });
 
-console.log(x)
+console.log(x);
