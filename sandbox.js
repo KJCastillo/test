687a62eb111583a02770588e62e5c2b5e00efd88
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-24 13:38:43 -0700

    map method

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,14 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
+const users = [
+    {name: "kevin", premium: true},
+    {name: 'castillo', premium: false},
+    {name: 'penny', premium: false},
+    {name: 'daisy', premium: true}
+];
+
+const x = users.filter(user => {
+    return user.premium
+});
+
+console.log(x)
