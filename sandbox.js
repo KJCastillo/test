3ea858edd593eb147efc02d7ee9022f54897266f
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-29 15:47:41 -0700

    sort method 2

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,31 @@
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+// const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-const x = scores.find((score) => {
-    return score > 50
+// scores.sort();
+// console.log(scores);
+
+// const names = ['kevin', 'penny', 'daisy', 'bjj'];
+
+// names.sort();
+// console.log(names);
+
+const players = [
+    {name: 'kevin', score: 20},
+    {name: 'penny', score: 10},
+    {name: 'daisy', score: 50}
+];
+
+// players.sort((a,b) => {
+//     if(a.score > b.score){
+//         return -1
+//     } else if (a.score < b.score){
+//         return 1
+//     } else {
+//         return 0
+//     }
+// });
+
+players.sort((a,b) => {
+    return a.score - b.score
 });
 
-console.log(x);
+console.log(players)
