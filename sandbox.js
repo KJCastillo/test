2dd83c1d3dec6fbed18e788dabda3ee81f4cc925
Kevin Castillo <kevincastillo318@gmail.com>
2022-03-29 15:32:33 -0700

    reduce method'

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,2 @@
-const scores = [
-    {player: 'kevin', score: 50},
-    {player: 'penny', score: 30},
-    {player: 'kevin', score: 70},
-    {player: 'johnny', score: 60}
-];
+const scores = [10, 20, 60, 40, 70, 90, 30];
 
-const x = scores.reduce((acc, curr) => {
-    if(curr.player === 'kevin'){
-        acc += curr.score
-    } return acc
-},0);
-
-console.log(x);
