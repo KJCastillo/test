13d5b7d8dae0616235c3944f38b6fa6934dc157e
Kevin Castillo <kevincastillo318@gmail.com>
2022-04-02 10:13:50 -0700

    timestamp

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,13 @@
+const before = new Date('March 18 2022 3:18:00');
 const now = new Date();
 
-console.log(now);
+const diff = now.getTime() - before.getTime();
 
-console.log('getFullYear', now.getFullYear());
-console.log('getMonth', now.getMonth());
-console.log('getDate', now.getDate());
-console.log('getDay', now.getDay());
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
+
+console.log(`the blog was written ${days} days ago`);
+
+const timestamp = 1675938474990;
+console.log(new Date(timestamp));
