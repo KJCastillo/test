78da710f66feaaa1b5a7bc519e73060adb473c6b
Kevin Castillo <kevincastillo318@gmail.com>
2022-04-06 19:36:21 -0700

    async test

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,17 @@
 //fetch api
 
-fetch('todos.json').then((response) => {
-    return response.json();
-}).then(data => {
-    console.log(data)
-}).catch((err) => {
-    console.log('rejected', err);
-});
+// fetch('todos.json').then((response) => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data)
+// }).catch((err) => {
+//     console.log('rejected', err);
+// });
+
+const getTodos = async () => {
+    const response = await fetch("todos.json");
+    const data = await response.json();
+    console.log(data);
+};
+
+getTodos();
