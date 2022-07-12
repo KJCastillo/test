247cbdab05ce1479b982d1bec3fde037dc419332
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-12 12:19:31 -0700

    storing array into local storage

--- readme.md
+++ readme.md
@@ -19,3 +19,20 @@ localStorage.removeItem('age');
 localStorage.clear();
 
 //type local storage to directly to console
+
+const todos = [
+    {text: 'test', author: 'kevin'},
+    {text: 'test2', author: 'kevin2'},
+    {text: 'test3', author: 'kevin3'},
+];
+
+//console.log(JSON.stringify(todos))
+
+localStorage.setItem('todos', JSON.stringify(todos))
+//make array into string and then store
+
+
+const stored = localStorage.getItem('todos')
+console.log(JSON.parse(stored))
+//gets data back from local storage but as an array again
+//create var to get data, then parse from JSON to an array again
--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,16 @@
-//store local storage
-localStorage.setItem('name', 'Kevin');
-localStorage.setItem('age', 32);
+const todos = [
+    {text: 'test', author: 'kevin'},
+    {text: 'test2', author: 'kevin2'},
+    {text: 'test3', author: 'kevin3'},
+];
 
-//get data from local storage
-let name = localStorage.getItem('name')
-let age = localStorage.getItem('age')
+//console.log(JSON.stringify(todos))
 
-console.log(`${name} is ${age} years old and will be a developer in 2022`)
-
-//deleting data from local storage
-localStorage.removeItem('age');
+localStorage.setItem('todos', JSON.stringify(todos))
+//make array into string and then store
 
 
-//type local storage to directly to console
+const stored = localStorage.getItem('todos')
+console.log(JSON.parse(stored))
+//gets data back from local storage but as an array again
+//create var to get data, then parse from JSON to an array again
