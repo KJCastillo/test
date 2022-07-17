ce50e2dfbb708e2c75a149bb327e555988ae1646
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-17 12:01:41 -0700

    delete data from db

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,59 @@
 //noSQL database
-const list = document.querySelector('ul');
-const form = document.querySelector('form');
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
 
-const addRecipe = (recipe) => {
-    let time = recipe.created_at.toDate();
-    let html = `
-    <li>
+const addRecipe = (recipe, id) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+    <li data-id="${id}">
         <div>${recipe.title}</div>
         <div>${time}</div>
+        <button class="btn btn-danger btn-sm my-2">delete</button>
     </li>
     `;
-    list.innerHTML += html
-}
+  list.innerHTML += html;
+};
 
 //get documents
-db.collection('recipes')
-   .get()
-   .then((snapshot) => {
-      snapshot.docs.forEach((doc) => {
-         addRecipe(doc.data());
-      });
-   })
-   .catch((err) => {
-      console.log(err);
-   });
+db.collection("recipes")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      addRecipe(doc.data(), doc.id);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //add documents
-form.addEventListener('submit', e => {
-    e.preventDefault();
-  
-    const now = new Date();
-    const recipe = {
-      title: form.recipe.value,
-      created_at: firebase.firestore.Timestamp.fromDate(now)
-    };
-  
-    db.collection('recipes').add(recipe).then(() => {
-      console.log('recipe added');
-    }).catch(err => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch((err) => {
       console.log(err);
     });
-  });
+});
+
+//delete data
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    db.collection('recipes').doc(id).delete().then(() => {
+        console.log('recipe deleted')
+    });
+    //looks into db for 'recipes' colletion and .doc looks into each document
+    //for that specific id
+  }
+});
