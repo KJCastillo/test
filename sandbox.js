2ebec7e4cc00660ce722960afac74c3e612556cf
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-16 18:10:20 -0700

    data appended to html

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,21 @@
 //noSQL database
+const list = document.querySelector('ul');
+
+const addRecipe = (recipe) => {
+    let html = `
+    <li>
+        <div>${recipe.title}</div>
+    </li>
+    `;
+    list.innerHTML += html
+}
+
+
 db.collection('recipes')
    .get()
    .then((snapshot) => {
       snapshot.docs.forEach((doc) => {
-         console.log(doc.data());
+         addRecipe(doc.data());
       });
    })
    .catch((err) => {
