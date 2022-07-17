7a9a750985752ba76e8af1b763e94f7f946467cd
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-16 18:38:11 -0700

    add recipe

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 //noSQL database
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
 const addRecipe = (recipe) => {
     let time = recipe.created_at.toDate();
@@ -12,6 +13,7 @@ const addRecipe = (recipe) => {
     list.innerHTML += html
 }
 
+//get documents
 db.collection('recipes')
    .get()
    .then((snapshot) => {
@@ -22,3 +24,20 @@ db.collection('recipes')
    .catch((err) => {
       console.log(err);
    });
+
+//add documents
+form.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    const now = new Date();
+    const recipe = {
+      title: form.recipe.value,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+  
+    db.collection('recipes').add(recipe).then(() => {
+      console.log('recipe added');
+    }).catch(err => {
+      console.log(err);
+    });
+  });
