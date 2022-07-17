684f013918808b2a7a46d601b36be262bc6c0b56
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-16 17:58:49 -0700

    connected firebase/firestore

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,11 @@
-const todos = [
-    {text: 'test', author: 'kevin'},
-    {text: 'test2', author: 'kevin2'},
-    {text: 'test3', author: 'kevin3'},
-];
-
-//console.log(JSON.stringify(todos))
-
-localStorage.setItem('todos', JSON.stringify(todos))
-//make array into string and then store
-
-
-const stored = localStorage.getItem('todos')
-console.log(JSON.parse(stored))
-//gets data back from local storage but as an array again
-//create var to get data, then parse from JSON to an array again
+//noSQL database
+db.collection('recipes')
+   .get()
+   .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+         console.log(doc.data());
+      });
+   })
+   .catch((err) => {
+      console.log(err);
+   });
