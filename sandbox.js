bd7e52e15319e54693f0e3f0c8f46e025fd3bb1d
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-27 09:45:40 -0700

    symbols()

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,3 @@
-//rest
-const double = (...nums) => {
-  console.log(nums);
-  return nums.map(num => num*2)
-}
-
-const result = double(1,3,5,7,9,2,4,6,8)
-
-console.log(result)
-
-//spread syntax (arrays)
-const people = ['kevin', 'penny', 'daisy']
-const members = ['mario', 'jose', ...people]
-console.log(members)
-
-//spread syntax (objects)
-const person = {name: 'kevin', age: 32, job: 'developer'}
-const personClone = {...person, location: 'san diego'}
-console.log(personClone)
+//symbols
+const symbolOne = Symbol()
+const symbolTwo = Symbol()
