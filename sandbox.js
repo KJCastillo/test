cf5d130727bbdda2936703a182bdf1570cc0cac7
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-28 14:44:38 -0700

    .babelrc made

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,5 @@
 //create package.json file by typing npm init in terminal
 
 //create babel by typing npm install @babel/core @babel/cli --save-dev
+
+//create preset-env by typing npm install @babel/preset-env --save-dev
